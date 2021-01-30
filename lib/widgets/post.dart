0ee7028eddbd5cb3a91664f99ad309ad2a7926cf
Tyger Hwang <boglbbogl@gmail.com>
2021-01-31 00:43:55 +0900

    instagram_clone post.dart ver-2

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,5 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/constants/common_size.dart';
+import 'package:instagram_clone/widgets/my_progress_indicator.dart';
+
+import 'rounded_avatar.dart';
 
 class Post extends StatelessWidget {
   final int index;
@@ -12,23 +16,66 @@ class Post extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (size == null) size = MediaQuery.of(context).size;
 
-    if(size==null)
-      size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        _postHeader(),
+        _postImage(),
+        _postActions(),
+      ],
+    );
+  }
 
+  Row _postActions() {
+    return Row(
+        children: <Widget>[
+          IconButton(
+              icon: ImageIcon(AssetImage('assets/images/bookmark.png')),
+              color: Colors.black87,
+              onPressed: null),
+          IconButton(
+              icon: ImageIcon(AssetImage('assets/images/comment.png')),
+              color: Colors.black87,
+              onPressed: null),
+          IconButton(
+              icon: ImageIcon(AssetImage('assets/images/direct_message.png')),
+              color: Colors.black87,
+              onPressed: null),
+          Spacer(),
+          IconButton(
+              icon: ImageIcon(AssetImage('assets/images/heart_selected.png')),
+              color: Colors.black87,
+              onPressed: null),
+        ],
+      );
+  }
+
+  Widget _postHeader() {
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(common_xxs_gap),
+          child: RoundedAvatar(),
+        ),
+        Expanded(child: Text('username')),
+        IconButton(
+            icon: Icon(
+              Icons.more_horiz,
+              color: Colors.black87,
+            ),
+            onPressed: null)
+      ],
+    );
+  }
+
+  CachedNetworkImage _postImage() {
     return CachedNetworkImage(
       imageUrl: 'https://picsum.photos/id/$index/200/300',
       placeholder: (BuildContext context, String url) {
-        return Container(
-          width: size.width,
-            height: size.height,
-            child: Center(
-                child: SizedBox(
-                    height: 60,
-                    width: 60,
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.black87,
-                    ))));
+        return MyProgressIndicator(
+          containerSize: size.width,
+        );
       },
       imageBuilder: (BuildContext context, ImageProvider imageProvider) {
         return AspectRatio(
