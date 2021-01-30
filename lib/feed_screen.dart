0f1dcac2e396e111f8380d931322d0ee9169d55b
Tyger Hwang <boglbbogl@gmail.com>
2021-01-30 23:51:00 +0900

    instagram_clone post.dart

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,17 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'widgets/post.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'instagram',
-          style: TextStyle(
-            fontFamily: 'VeganStyle'
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+          onPressed: null,
+          icon: Icon(
+            CupertinoIcons.switch_camera_solid,
+            color: Colors.black87,
           ),
         ),
+        middle: Text(
+          'instagram',
+          style: TextStyle(
+            fontFamily: 'VeganStyle',
+            color: Colors.black87,
+          ),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              onPressed: null,
+              icon: ImageIcon(
+                AssetImage('assets/images/actionbar_camera2.png'),
+                color: Colors.black87,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: feedListBuilder,
       ),
     );
   }
+
+  Widget feedListBuilder(BuildContext context, int index) {
+    return Post(index);
+  }
 }
+
+
