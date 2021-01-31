3d11085d96e528c657596115114211610d33f76c
Tyger Hwang <boglbbogl@gmail.com>
2021-01-31 16:03:56 +0900

    instagram_clone auth_screen ver - 2

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
+import 'package:instagram_clone/widgets/sigh_up_form.dart';
+import 'package:instagram_clone/widgets/sign_in_form.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -6,8 +9,44 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+
+  Widget signUpForm = SignUpForm();
+  Widget signInForm = SignInForm();
+
+  Widget currentWidgets;
+
+  @override
+  void initState() {
+    if(currentWidgets == null)
+      currentWidgets = signUpForm;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            AnimatedSwitcher(
+                child: currentWidgets,
+              duration: duration,
+            ),
+            Container(
+              child: FlatButton(onPressed: () {
+                setState(() {
+                  if(currentWidgets is SignUpForm){
+                    currentWidgets = signInForm;
+                  } else{
+                    currentWidgets = signUpForm;
+                  }
+                });
+              },
+                  child: Text('go to Sign up')),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
