0b8a97c09f83f7873b92e2e0821350cd3da8f68b
Tyger Hwang <boglbbogl@gmail.com>
2021-01-31 16:59:16 +0900

    instagram_clone fade_stack delect

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/widgets/fade_stack.dart';
+import 'package:instagram_clone/screens/profile_screen.dart';
+import 'package:instagram_clone/widgets/sigh_up_form.dart';
+import 'package:instagram_clone/widgets/sign_in_form.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
   _AuthScreenState createState() => _AuthScreenState();
 }
 
-class _AuthScreenState extends State<AuthScreen>{
-  int selectedForm = 0;
+class _AuthScreenState extends State<AuthScreen> {
+  Widget signUpForm = SignUpForm();
+  Widget signInForm = SignInForm();
+
+  Widget currentWidget;
+
+  @override
+  void initState() {
+    if(currentWidget == null) currentWidget = signUpForm;
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,21 +27,41 @@ class _AuthScreenState extends State<AuthScreen>{
       body: SafeArea(
         child: Stack(
           children: <Widget>[
-            FadeStack(
-              selectedForm: selectedForm,
+            AnimatedSwitcher(
+              child: currentWidget,
+              duration: duration,
             ),
-            Container(
-              child: FlatButton(
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                color: Colors.white,
+                child: FlatButton(
                   onPressed: () {
-                setState(() {
-                  if(selectedForm == 0){
-                    selectedForm = 1;
-                  } else{
-                    selectedForm = 0;
-                  }
-                });
-              },
-                  child: Text('go to Sign up')),
+                    setState(() {
+                      if (currentWidget is SignUpForm) {
+                        currentWidget = signInForm;
+                      } else {
+                        currentWidget = signUpForm;
+                      }
+                    });
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                        text: (currentWidget is SignUpForm) ? 'Already have an account? ':"Don't have an account? ",
+                        style: TextStyle(color: Colors.grey),
+                        children: [
+                          TextSpan(
+                            text: (currentWidget is SignUpForm) ? 'Sign In' : 'Sign Up',
+                            style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ]),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
